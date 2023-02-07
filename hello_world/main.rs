acdaa4655908cd19a43640f6583a18baf7e7e960
SobiX@Mendel <sobhanee@email.com>
2023-02-07 23:57:27 +0330

    Hello, world! Hello, rust

---
+++ hello_world/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("Hello, world!\n");
+    println!("If \"Hello, world!\" did print, congratulations!\nYou’ve officially written a Rust program.\nThat makes you a Rust programmer—welcome!");
+}
