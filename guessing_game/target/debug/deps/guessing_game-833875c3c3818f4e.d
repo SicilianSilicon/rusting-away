129dff475bc74e98b89256ff7b624baa6c24f2df
SobiX@Mendel <sobhanee@email.com>
2023-02-07 23:58:05 +0330

    Beginning to rust

---
+++ guessing_game/target/debug/deps/guessing_game-833875c3c3818f4e.d
@@ -0,0 +1,5 @@
+/home/sobix/Code/rustacean/projects/guessing_game/target/debug/deps/guessing_game-833875c3c3818f4e: src/main.rs
+
+/home/sobix/Code/rustacean/projects/guessing_game/target/debug/deps/guessing_game-833875c3c3818f4e.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/sobix/Code/rustacean/projects/guessing_game/target/debug/guessing_game: /home/sobix/Code/rustacean/projects/guessing_game/src/main.rs
