5229caaad36b64104cbcd4cda3f85dfafcfed3a4
SobiX@Home <siciliansilicon@gmail.com>
2023-02-11 00:11:01 +0330

    small bugs removed

---
+++ guessing_game/target/debug/deps/guessing_game-5954daaa49631f43.d
@@ -0,0 +1,5 @@
+/home/sobix/Code/rustacean/projects/guessing_game/target/debug/deps/guessing_game-5954daaa49631f43.rmeta: src/main.rs
+
+/home/sobix/Code/rustacean/projects/guessing_game/target/debug/deps/guessing_game-5954daaa49631f43.d: src/main.rs
+
+src/main.rs:
